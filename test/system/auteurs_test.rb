900e31f3629054b00a3a21441cf248e573e3abbe
Sebremy <gauthier-seb@orange.fr>
2018-02-22 16:31:09 +0100

    modele_auteur

---
+++ test/system/auteurs_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class AuteursTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit auteurs_url
+  #
+  #   assert_selector "h1", text: "Auteur"
+  # end
+end
